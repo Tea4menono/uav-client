0918e9c7fe480cade5bd0a58a04e9eb8d03c2410
helloloop <1204386811@qq.com>
2023-05-30 16:09:24 -0600

    123

--- test.py
+++ test.py
@@ -11,7 +11,10 @@ async def connect_websocket():
         while True:
             # Send location data to the server
             if flight.home:
-                await websocket.send(json.dumps({"latitude":flight.home.lat,"longtitude":flight.home.lon,"altitude":flight.home.alt}))
+                print(flight.home,12321)
+                await websocket.send(json.dumps({"latitude":flight.home.lat,
+                                                 "longtitude":flight.home.lon,
+                                                 "altitude":flight.home.alt}))
 
 
             # Wait for incoming message from the server
