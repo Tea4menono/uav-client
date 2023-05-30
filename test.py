e2ef7c37204027386bfef37a5f80b7bb438ad811
helloloop <1204386811@qq.com>
2023-05-30 16:35:10 -0600

    123

--- test.py
+++ test.py
@@ -2,25 +2,39 @@ import asyncio
 import websockets
 import flight
 import json
+
+async def send_location(websocket):
+    while True:
+        # Send location data to the server
+        if flight.home:
+            await websocket.send(json.dumps({"latitude":flight.home.lat,
+                                             "longitude":flight.home.lon,
+                                             "altitude":flight.home.alt}))
+        await asyncio.sleep(1)  # sleep for 1 second
+
+async def receive_messages(websocket):
+    while True:
+        # Wait for incoming message from the server
+        response = await websocket.recv()
+        response = json.loads(response)
+        print(f"Received: {response}")
+        flight.mission(response)
+
 async def connect_websocket():
     uri = "ws://3.139.94.118:3000"  # Replace with your WebSocket server URL
 
     async with websockets.connect(uri) as websocket:
         print("Connected to WebSocket server")
 
-        while True:
-            # Send location data to the server
-            if flight.vehicle.location.global_relative_frame:
-                await websocket.send(json.dumps({"latitude":flight.vehicle.location.global_relative_frame.lat,
-                                                 "longitude":flight.vehicle.location.global_relative_frame.lon,
-                                                 "altitude":flight.vehicle.location.global_relative_frame.alt}))
-                await asyncio.sleep(1)
+        sender = asyncio.create_task(send_location(websocket))
+        receiver = asyncio.create_task(receive_messages(websocket))
+        
+        done, pending = await asyncio.wait(
+            [sender, receiver],
+            return_when=asyncio.FIRST_COMPLETED,
+        )
 
-
-            # Wait for incoming message from the server
-            response = await websocket.recv()
-            response=json.loads(response)
-            print(f"Received: {response}")
-            flight.mission(response)
+        for task in pending:
+            task.cancel()
 
 asyncio.run(connect_websocket())
